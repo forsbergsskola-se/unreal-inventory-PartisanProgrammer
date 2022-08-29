995f43fb742984499abeee9f0ca0cc5cb137706c
Oliver Källerfors <idetorka@hotmail.com>
2022-08-29 19:30:23 +0200

    Restructured Project

---
+++ FirstTestProject/Source/FirstTestProject/FirstCPPClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class FIRSTTESTPROJECT_API FirstCPPClass
+{
+public:
+	FirstCPPClass();
+	~FirstCPPClass();
+};
