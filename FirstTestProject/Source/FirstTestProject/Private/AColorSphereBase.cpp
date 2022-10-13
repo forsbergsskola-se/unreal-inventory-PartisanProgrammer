0142b71c329ddabc38a6840a96ab660081000552
Oliver Källerfors <idetorka@hotmail.com>
2022-10-13 09:14:21 +0200

    Implemented Multiplayer color change with c++

---
+++ FirstTestProject/Source/FirstTestProject/Private/AColorSphereBase.cpp
@@ -0,0 +1,73 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AColorSphereBase.h"
+
+#include "Net/UnrealNetwork.h"
+
+
+void AAColorSphereBase::Interact_Implementation(ACharacter* Interactor){
+	IIInteract::Interact_Implementation(Interactor);
+}
+
+// Sets default values
+AAColorSphereBase::AAColorSphereBase(){
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+	SphereComponent = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Sphere"));
+}
+
+// Called when the game starts or when spawned
+void AAColorSphereBase::BeginPlay(){
+	Super::BeginPlay();
+	Material = SphereComponent->GetMaterial(0);
+	MID = SphereComponent->CreateDynamicMaterialInstance(0,Material);
+}
+
+void AAColorSphereBase::OnRepCurrentColorChanged() const{
+	MID-> SetVectorParameterValue("Color", CurrentColor);
+}
+
+void AAColorSphereBase::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	DOREPLIFETIME(AAColorSphereBase, CurrentColor);
+}
+
+// Called every frame
+void AAColorSphereBase::Tick(float DeltaTime){
+	Super::Tick(DeltaTime);
+
+}
+
+void AAColorSphereBase::RandomizeColor(){
+		CurrentColor = FLinearColor(FMath::FRand(), FMath::FRand(), FMath::FRand(), 1.0f);
+		OnRepCurrentColorChanged();
+	
+}
+
+void AAColorSphereBase::ChangeColor(){
+	//Random linearcolor
+	
+	
+	// switch (CurrentColor){ case EColors::RED:
+	// 	CurrentColor = COLOR_RED;
+	// 	break;
+	// case EColors::GREEN:
+	// 	CurrentColor = EColors::GREEN;
+	// 	break;
+	// case EColors::BLUE:
+	// 	CurrentColor = EColors::BLUE;
+	// 	break;
+	// case EColors::YELLOW:
+	// 	CurrentColor = EColors::YELLOW;
+	// 	break;
+	// case EColors::PURPLE:
+	// 	CurrentColor = EColors::PURPLE;
+	// 	break;
+	// default: ;
+	// }
+
+	OnRepCurrentColorChanged();
+
+}
+
---
+++ FirstTestProject/Source/FirstTestProject/Public/AColorSphereBase.h
@@ -0,0 +1,58 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "IInteract.h"
+#include "GameFramework/Actor.h"
+#include "AColorSphereBase.generated.h"
+UENUM(BlueprintType)
+enum class EColors:uint8{
+	RED,
+	GREEN,
+	BLUE,
+	YELLOW,
+	PURPLE,
+};
+UCLASS()
+class FIRSTTESTPROJECT_API AAColorSphereBase : public AActor, public IIInteract{
+public:
+	virtual void Interact_Implementation(ACharacter* Interactor) override;
+
+private:
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	
+	UMaterialInstanceDynamic* MID;
+	UMaterialInterface* Material;
+	AAColorSphereBase();
+	
+	
+	
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+	UPROPERTY(ReplicatedUsing=OnRepCurrentColorChanged)
+	FLinearColor CurrentColor;
+	UFUNCTION()
+	void OnRepCurrentColorChanged() const;
+	UPROPERTY(EditDefaultsOnly, Category = "Color")
+    UStaticMeshComponent* SphereComponent;
+	UPROPERTY(EditDefaultsOnly, Category = "Color")
+	TMap<EColors,UMaterialInterface*> MaterialMap;
+
+private:
+	virtual void GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const override;
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	
+	UFUNCTION(BlueprintCallable, Category = "Color")
+	void RandomizeColor();
+	UFUNCTION(BlueprintCallable, Category = "Color")
+	void ChangeColor();
+
+};
